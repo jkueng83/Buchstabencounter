b39205b932a6effb242b58129f6ebb3f06a1e87f
jkueng83 <johannes.kueng@gmx.at>
2020-02-22 09:57:20 +0100

    init Buchstabencounter

---
+++ src/at/jku/Main.java
@@ -0,0 +1,9 @@
+package at.jku;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Das ist der Buchstabencounter!");
+    }
+}
